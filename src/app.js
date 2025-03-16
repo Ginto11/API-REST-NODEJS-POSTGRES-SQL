150c5fd5f8fa88d18313d805b9858b6222dc2645
Ginto11 <salinitosnelson@gmail.com>
2025-03-16 14:59:51 -0500

    Desabilitando cors en la api

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import express from 'express';
 import { PORT_LOCAL } from './config.js';
 import  userRouter  from './routes/users.routes.js';
 import morgan from 'morgan';
+import cors from 'cors'
+
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
+
 app.use(userRouter);
+
+
 
 app.listen(PORT_LOCAL);
 console.log("Servidor en puerto", PORT_LOCAL);
